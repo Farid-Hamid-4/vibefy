12eb719a2c235dc8fae0488ff58d8c790363cd71
Farid Hamid <fhamid@unionvillecollege.com>
2024-04-01 14:52:21 -0400

    Made the login page responsive for mobile use

--- src/Login.js
+++ src/Login.js
@@ -5,23 +5,23 @@ import Animation from "./Animation3.json";
 import "./Login.css";
 
 const CLIENT_ID = "2c11048635dd4d6f928a6a38371cbfe9";
-// const REDIRECT_URI = "https://main.d2xqpnct98klit.amplifyapp.com/";
-const REDIRECT_URI = "http://localhost:3000/";
+const REDIRECT_URI = "https://main.d2xqpnct98klit.amplifyapp.com/";
+// const REDIRECT_URI = "http://localhost:3000/";
 const SCOPES = "user-top-read playlist-modify-private streaming user-read-email user-read-private user-library-read user-library-modify user-read-playback-state user-modify-playback-state";
 const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${SCOPES}`;
 
 export default function Login() {
   return (
     <Container fluid>
-      <Row id="background" style={{ height: "100vh" }}>
-        <Col id="login" className="d-flex flex-column justify-content-center align-items-start ps-5">
+      <Row className="login-background">
+        <Col id="login-headings" className="d-flex flex-column justify-content-center align-items-start ps-5">
           <h1>Vibefy</h1>
-          <h2>Get recommended tracks based on a song of your choice</h2>
+          <h2>Curate a playlist based on a song of your choice</h2>
           <Button active id="login-button" href={AUTH_URL}>
             Login with Spotify
           </Button>
         </Col>
-        <Col className="d-flex justify-content-center">
+        <Col className="d-none d-md-flex justify-content-center">
           <Lottie loop={true} animationData={Animation}/>
         </Col>
       </Row>
